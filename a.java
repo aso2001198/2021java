657182a1cadc4346ef9c2214c2348ecb01d5c6ed
aso2001198 <87457063+aso2001198@users.noreply.github.com>
2021-07-15 11:23:37 +0900

    Add files via upload

---
+++ a.java
@@ -0,0 +1,22 @@
+package Main;
+
+import java.util.Scanner;
+
+public class a{
+    public static void main(String[] args){
+System.out.println("戦艦ゲーム");
+        
+
+
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("爆弾のX座標を入力してください(1-5)");
+        int x = scanner.nextInt();
+        System.out.println("爆弾のY座標を入力してください(1-5)");
+        int y = scanner.nextInt();
+
+    }
+}
